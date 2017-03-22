8cb1538abdec7cf2637f23d1f4bbc202154e46f3
gimenez julien <julien.gimenez@nestle.com>
2017-03-22 13:49:31 +0100

    Cell constraints

--- Win Fitness/WFNewsTableViewCell.m
+++ Win Fitness/WFNewsTableViewCell.m
@@ -15,7 +15,6 @@
 
 @property(nonatomic,strong) UILabel *dayLabel;
 @property(nonatomic,strong) UITextView *bodyTextView;
-@property(nonatomic,strong) UIView *supportView;
 @property(nonatomic,strong) WFFacebookFeedModel *facebookModel;
 
 @end
@@ -32,6 +31,7 @@
     if (!_newsImage) {
         _newsImage = [[UIImageView alloc]initWithFrame:CGRectZero];
         _newsImage.contentMode = UIViewContentModeScaleAspectFill;
+        _newsImage.clipsToBounds = YES;
     }
     return _newsImage;
 }
@@ -39,17 +39,10 @@
 - (UILabel *)dayLabel {
     if (!_dayLabel) {
         _dayLabel = [[UILabel alloc]initWithFrame:CGRectZero];
-        _dayLabel.backgroundColor = [UIColor redColor];
+        _dayLabel.font = [UIFont boldSystemFontOfSize:12];
+        _dayLabel.backgroundColor = [UIColor whiteColor];
     }
     return _dayLabel;
-}
-
-- (UIView *)supportView {
-    if (!_supportView) {
-        _supportView = [[UIView alloc]initWithFrame:CGRectZero];
-        _supportView.backgroundColor = [UIColor whiteColor];
-    }
-    return _supportView;
 }
 
 - (UITextView *)bodyTextView {
@@ -58,6 +51,8 @@
         _bodyTextView.scrollEnabled = NO;
         _bodyTextView.userInteractionEnabled = NO;
         _bodyTextView.textAlignment = NSTextAlignmentJustified;
+        _bodyTextView.font = [UIFont systemFontOfSize:14];
+        _bodyTextView.backgroundColor = [UIColor whiteColor];
     }
     return _bodyTextView;
 }
@@ -76,37 +71,29 @@
 
 - (void)setupView {
     [self addSubview:self.newsImage];
-    [self addSubview:self.supportView];
-    [self.supportView addSubview:self.dayLabel];
-    [self.supportView addSubview:self.bodyTextView];
+    [self addSubview:self.dayLabel];
+    [self addSubview:self.bodyTextView];
 }
 
 - (void)setupConstraints {
     [self.newsImage mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.mas_left);
         make.top.equalTo(self.mas_top);
         make.bottom.equalTo(self.mas_bottom);
-        make.left.equalTo(self);
         make.width.equalTo(@75);
     }];
 
-    [self.supportView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.mas_top);
-        make.bottom.equalTo(self.mas_bottom);
-        make.left.equalTo(self.newsImage.mas_right);
-        make.right.equalTo(self.mas_right);
-    }];
-
     [self.dayLabel mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.supportView.mas_top);
-        make.left.equalTo(self.supportView.mas_left).offset(5);
-        make.height.equalTo(@25);
+        make.left.equalTo(self.newsImage.mas_right).offset(5);
+        make.top.equalTo(self.mas_top);
+        make.height.equalTo(@20);
     }];
     
     [self.bodyTextView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.left.equalTo(self.newsImage.mas_right);
         make.top.equalTo(self.dayLabel.mas_bottom);
-        make.bottom.equalTo(self.supportView.mas_bottom);
-        make.left.equalTo(self.supportView.mas_left);
-        make.right.equalTo(self.supportView.mas_right);
+        make.right.equalTo(self.mas_right);
+        make.bottom.equalTo(self.mas_bottom);
     }];
 }
 
