7c0816fe3d1d36cf1660bdd169260a206a253d45
coincoin38 <juliengimenez38@gmail.com>
2017-03-25 10:45:08 +0100

    Add scrollView

--- Win Fitness/WFNewsViewController.m
+++ Win Fitness/WFNewsViewController.m
@@ -13,25 +13,40 @@
 
 @interface WFNewsViewController ()
 
-@property(nonatomic,strong) WFCustomTextView *bodyTextView;
 @property(nonatomic,strong) UIImageView *newsImage;
+@property(nonatomic,strong) UIScrollView *newsScrollView;
+@property(nonatomic,strong) UIView *contentView;
+@property(nonatomic,strong) WFCustomTextView *bodyTextView;
 @property(nonatomic,strong) WFFacebookFeedModel *news;
 
 @end
 
 @implementation WFNewsViewController
 
+#pragma mark - Init
+
+- (instancetype)initWithNews:(WFFacebookFeedModel *)news {
+    self = [super init];
+    
+    if (self) {
+        _news = news;
+    }
+    return self;
+}
+
+#pragma mark - View Life Cycle
+
 - (void)viewDidLoad {
     [super viewDidLoad];
 
     self.view.backgroundColor = [UIColor whiteColor];
-
     [WFDownloadImageService downloadImage:self.news.full_picture forUIImageView:self.newsImage];
     self.bodyTextView.text = self.news._description;
-    
     [self setupView];
     [self setupConstraints];
 }
+
+#pragma mark - User Interface Elements
 
 - (UIImageView *)newsImage {
     if (!_newsImage) {
@@ -42,45 +57,58 @@
     return _newsImage;
 }
 
-- (UITextView *)bodyTextView {
+- (WFCustomTextView *)bodyTextView {
     if (!_bodyTextView) {
         _bodyTextView = [[WFCustomTextView alloc]initWithFrame:CGRectZero];
     }
     return _bodyTextView;
 }
 
-
-- (void)setupView {
-    [self.view addSubview:self.newsImage];
-    [self.view addSubview:self.bodyTextView];
+- (UIScrollView *)newsScrollView {
+    if (!_newsScrollView) {
+        _newsScrollView = [[UIScrollView alloc]initWithFrame:CGRectZero];
+    }
+    return _newsScrollView;
 }
 
+- (UIView *)contentView {
+    if (!_contentView) {
+        _contentView = [[UIView alloc]initWithFrame:CGRectZero];
+    }
+    return _contentView;
+}
+
+- (void)setupView {
+    [self.view addSubview:self.newsScrollView];
+    [self.newsScrollView addSubview:self.contentView];
+    [self.contentView addSubview:self.newsImage];
+    [self.contentView addSubview:self.bodyTextView];
+}
+
+#pragma mark - Constraints
+
 - (void)setupConstraints {
+    [self.newsScrollView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.view);
+    }];
+    
+    [self.contentView mas_makeConstraints:^(MASConstraintMaker *make) {
+        make.edges.equalTo(self.newsScrollView);
+        make.width.equalTo(self.newsScrollView);
+        make.bottom.equalTo(self.bodyTextView.mas_bottom).offset(5);
+    }];
+    
     [self.newsImage mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(self.view.mas_left);
-        make.top.equalTo(self.view.mas_top);
-        make.right.equalTo(self.view.mas_right);
+        make.left.equalTo(self.contentView.mas_left);
+        make.top.equalTo(self.contentView.mas_top);
+        make.right.equalTo(self.contentView.mas_right);
     }];
 
     [self.bodyTextView mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.left.equalTo(self.view.mas_left).offset(5);
+        make.left.equalTo(self.contentView.mas_left).offset(5);
         make.top.equalTo(self.newsImage.mas_bottom).offset(5);
-        make.right.equalTo(self.view.mas_right).offset(-5);
+        make.right.equalTo(self.contentView.mas_right).offset(-5);
     }];
-}
-
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
-}
-
-- (instancetype)initWithNews:(WFFacebookFeedModel *)news {
-    self = [super init];
-
-    if (self) {
-        _news = news;
-    }
-    return self;
 }
 
 @end
